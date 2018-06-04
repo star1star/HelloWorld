f4b666baa0e018c08bd953b493aad1f27727b92a
Madhu Nadikota <madhu.nadikota@rmit.edu.au>
2018-06-05 05:57:29 +1000

    rebase test 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String str = "hello world";
+		String str = "hello world"; --Madhu
 		
 		System.out.println(str);
 		
